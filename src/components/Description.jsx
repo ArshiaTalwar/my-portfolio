4c616e153fbe128caa610e9b654760069587213f
Arshia Talwar <arshiatalwar17@gmail.com>
2025-04-07 00:42:04 +0530

    Initial commit

---
+++ src/components/Description.jsx
@@ -0,0 +1,23 @@
+export default function Description() {
+  return (
+    <section
+      id="description"
+      className="EXPERIENCE p-5 mx-auto mb-1 min-h-[70vh] max-w-[1300px] text-center font-['Poppins'] max-sm:p-2 max-sm:mx-5"
+    >
+      <div className="WRAPPER mt-10">
+        <h1 className=" dark:text-slate-300 text-center font-extrabold text-5xl mb-5 max-sm:text-4xl">
+         About me
+        </h1>
+        <p className="ABOUT h-auto text-xl max-sm:text-[15px] bg-clip-text text-transparent bg-gradient-to-r from-[#00040f] to-slate-500 dark:from-slate-500 dark:to-slate-200 max-w-470 mt-5">
+          I'm a tech enthusiast, a pre-final year B.Tech student based in
+          Delhi-NCR who is looking for opportunities to grow and learn. I'm a
+          Frontend Web Developer focused on building and managing the Front-end
+          of Websites and Web Applications. Check out some of my work in the
+          Projects section. I'm open to job opportunities where I can
+          contribute, learn, and grow. If you have a good opportunity that
+          matches my skills and experience, then don't hesitate to contact me.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/constants/Constant.jsx
@@ -0,0 +1,57 @@
+// import C from "../assets/C.png";
+// import CPP from "../assets/CPP.png";
+import HTML from "../assets/HTML.png";
+import Github from "../assets/Github.png";
+import CSS from "../assets/CSS.png";
+import Python from "../assets/Python.png";
+import Javascript from "../assets/Javascript.png";
+import Git from "../assets/Git.png";
+import Vite from "../assets/Vite.png";
+import TailwindCSS from "../assets/TailwindCSS.png";
+import react from "../assets/react.png";
+import sql from "../assets/sql.png";
+import mongoDB from "../assets/mongoDB.png";
+
+import java from "../assets/java.png";
+import Sports from "../assets/Sports_Club.png";
+import powerbi from "../assets/powerbi.png";
+import GDSC from "../assets/GDSC.png";
+import KDE from "../assets/kde.png";
+import Oppia from "../assets/oppia.png";
+import firebase from "../assets/firebase.png";
+import GPT4 from "../assets/gpt4.jpg";
+import Portfolio from "../assets/portfolio.jpg";
+import Rocket from "../assets/Rocket.jpg";
+import Game from "../assets/Stone.jpg";
+import WellBeing from "../assets/well-being.jpg";
+import  typescript from "../assets/typescript.png";
+
+export {
+ 
+  HTML,
+  CSS,
+  firebase,
+  Github,
+  Git,
+  Vite,
+  TailwindCSS,
+  react,
+  Python,
+  Javascript,
+  typescript,
+
+
+  java,
+  mongoDB,
+  Sports,
+  powerbi,
+  sql,
+  GPT4,
+  Portfolio,
+  Rocket,
+  WellBeing,
+  Game,
+  GDSC,
+  KDE,
+  Oppia,
+};
